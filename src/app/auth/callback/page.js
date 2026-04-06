152024b38b28f768ad0038812a9d01a502cd52c6
fuckitoff22 <afulzele865@gmail.com>
2026-04-06 18:38:12 +0530

    Update page.js

--- src/app/auth/callback/page.js
+++ src/app/auth/callback/page.js
@@ -6,26 +6,4 @@ import { supabase } from "@/lib/supabase";
 export default function Callback() {
   return <div>Callback</div>;
 }
-  useEffect(() => {
-    const handleLogin = async () => {
-      try {
-        await supabase.auth.exchangeCodeForSession();
-
-        // ✅ ALWAYS SAFE
-        window.location.href = "/";
-        
-      } catch (err) {
-        console.error(err);
-        window.location.href = "/login";
-      }
-    };
-
-    handleLogin();
-  }, []);
-
-  return (
-    <div className="h-screen flex items-center justify-center">
-      Logging you in...
-    </div>
-  );
-}
+ 
