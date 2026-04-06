1138bdc3879cd28b7639810e66e0f0328503c326
fuckitoff22 <afulzele865@gmail.com>
2026-04-06 17:58:07 +0530

    Update page.js

--- src/app/auth/callback/page.js
+++ src/app/auth/callback/page.js
@@ -1,27 +1,37 @@
 "use client";
 
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 import { supabase } from "@/lib/supabase";
+import { useRouter } from "next/navigation";
 
-export default function AuthCallback() {
+export default function Callback() {
   const router = useRouter();
 
   useEffect(() => {
     const handleLogin = async () => {
       const { data, error } = await supabase.auth.getSession();
 
+      if (error) {
+        console.error(error);
+        router.replace("/login");
+        return;
+      }
+
       if (data?.session) {
-        // ✅ USER LOGGED IN
-        router.push("/");
+        // ✅ SUCCESS LOGIN
+        router.replace("/");
       } else {
-        // ❌ FAILED
-        router.push("/login");
+        // ❌ FAILED LOGIN
+        router.replace("/login");
       }
     };
 
     handleLogin();
   }, []);
 
-  return <p>Logging you in...</p>;
+  return (
+    <div className="h-screen flex items-center justify-center">
+      Logging you in...
+    </div>
+  );
 }
